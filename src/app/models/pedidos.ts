eb6355f8704390e7f45f856d36151504bd6c479b
Willy <U202015236@UPC.EDU.PE>
2022-10-17 20:59:29 -0500

    Developer

--- src/app/models/pedidos.ts
+++
@@ -1,7 +0,0 @@
-export class Pedidos {
-    id_Pedido: number | undefined;
-    id_Consumidor: any;
-    fecha_Pedido: string | undefined;
-    pago_Total: number | undefined;
-    idTienda: number | undefined;
-}
