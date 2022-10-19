44259174870b85dc4b157ccafbf21b0f2b0a30ab
Willy <U202015236@UPC.EDU.PE>
2022-10-19 18:19:23 -0500

    Cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-//swper
-import { SwiperModule } from 'swiper/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarPedidoComponent } from './components/listar-pedido/listar-pedido.component';
+import { AgregarPedidoComponent } from './components/agregar-pedido/agregar-pedido.component';
+import { EditarPedidoComponent } from './components/editar-pedido/editar-pedido.component';
+import { HomeComponent } from './components/home/home.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularMaterialModule } from './components/shared/angular-material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ImgComponent } from './components/img/img.component';
-import { ProductComponent } from './components/product/product.component';
-import { ProductsComponent } from './components/products/products.component';
-import { NavComponent } from './components/nav/nav.component';
-import { ReversePipe } from './pipes/reverse.pipe';
-import { TimeAgoPipe } from './pipes/time-ago.pipe';
-import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImgComponent,
-    ProductComponent,
-    ProductsComponent,
-    NavComponent,
-    ReversePipe,
-    TimeAgoPipe,
-    HighlightDirective,
+    ListarPedidoComponent,
+    AgregarPedidoComponent,
+    EditarPedidoComponent,
+    HomeComponent,
+    ToolbarComponent,
+    ListarPedidoComponent,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    AngularMaterialModule,
+    ReactiveFormsModule,
     HttpClientModule,
-    SwiperModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
