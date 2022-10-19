4e41d8a81c96149d89b041500e2e26053ab22af9
Willy <U202015236@UPC.EDU.PE>
2022-10-19 18:37:10 -0500

    Estableciendo Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-// import { PedidosComponent } from './pedidos/pedidos.component';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router';
-const routes: Routes = [];
+import { AgregarPedidoComponent } from './components/agregar-pedido/agregar-pedido.component';
+import { EditarPedidoComponent } from './components/editar-pedido/editar-pedido.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListarPedidoComponent } from './components/listar-pedido/listar-pedido.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'listas/pedidos', component: ListarPedidoComponent},
+  {path: 'base/pedido/nuevo', component: AgregarPedidoComponent},
+  {path: 'base/pedido/editar/:id_Pedido', component: EditarPedidoComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
