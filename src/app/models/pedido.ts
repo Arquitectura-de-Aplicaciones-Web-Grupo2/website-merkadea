c79c27ea72317706c27879175124c2394c011b55
Willy <U202015236@UPC.EDU.PE>
2022-10-19 18:37:41 -0500

    Establecer model Pedido

---
+++ src/app/models/pedido.ts
@@ -0,0 +1,9 @@
+export class Pedido {
+    id_Pedido: number | undefined;
+    id_Producto: number | undefined;
+    id_Consumidor: number | undefined;
+    id_Tienda: number | undefined;
+    cantidad: number | undefined;
+    precio: number | undefined;
+    fecha_Pedido: string | undefined;
+}
