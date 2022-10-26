795a197ee7fbd80c4e5bcb3c490ecfb4d6c2b3d4
NotoriousJu <109191816+Junior-Gallardo28@users.noreply.github.com>
2022-10-26 00:36:22 -0500

    Front add-products / localizar merkapoint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductsComponent } from './components/add-products/add-products.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
 import { HomeConsumidorComponent } from './components/home-consumidor/home-consumidor.component';
+import { BuscarNegocioComponent } from './components/buscar-negocio/buscar-negocio.component';
 
 const routes: Routes = [
   {path: 'inventory/add-products',component:ProductsComponent},
   {path: 'home/welcome',component:HomeComponent},
   {path: 'home/inventory/add-products', component: AddProductsComponent},
-  {path: 'home/welcome-consumidor', component: HomeConsumidorComponent}
+  {path: 'home/welcome-consumidor', component: HomeConsumidorComponent},
+  {path: 'find/locate-market', component:BuscarNegocioComponent}
 ];
 
 @NgModule({
---
+++ src/app/components/buscar-negocio/buscar-negocio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buscar-negocio',
+  templateUrl: './buscar-negocio.component.html',
+  styleUrls: ['./buscar-negocio.component.scss']
+})
+export class BuscarNegocioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
