a19756fb2fc2ae33df30cde82b6d2e2dcb029c2d
jhimy <u201818218@upc.edu.pe>
2022-11-03 02:15:06 -0500

    commir ddf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  token = '';
+  constructor(
+    private authService: AuthService,
+    private userService: UsersService
+  ) {}
   title = 'website-merkadea';
+
+  createUser() {
+    this.userService
+      .create({
+        name: 'Jhimy',
+        email: 'jhimyjhimy26@gmail.com',
+        password: '1212',
+      })
+      .subscribe((rta) => {
+        console.log(rta);
+      });
+  }
+  login() {
+    this.authService
+      .login('jhimyjhimy26@gmail.com', '1212')
+      .subscribe((rta) => {
+        console.log(rta.access_token);
+        this.token = rta.access_token;
+      });
+  }
+
+  getProfile() {
+    this.authService.profile(this.token).subscribe((profile) => {
+      console.log(profile);
+    });
+  }
 }
