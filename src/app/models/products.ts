678f569865b0104ebc9599e6e0a61f37dc787f82
NotoriousJu <109191816+Junior-Gallardo28@users.noreply.github.com>
2022-11-05 10:23:30 -0500

    a

--- src/app/models/products.ts
+++ src/app/models/products.ts
@@ -1,6 +1,6 @@
 export class Products {
-    id_producto !: number;
-    nombre_producto !: string;
-    precio_producto!: number;
-    descuento_producto!: number;
+    id_producto : number | undefined;
+    nombre_producto : string | undefined;
+    precio_producto: number | undefined;
+    descuento_producto: number | undefined;
 }
